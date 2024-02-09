f7be54ff1e68f6b47220e9fa82508576f7dad8f5
Utkarsh Mishra <31947259+ug-prog01@users.noreply.github.com>
2024-02-09 18:34:28 +0530

    Added UI to Rock Paper Scissor

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 let gameValues = ["Rock", "Paper", "Scissor"];
 
+let roundsCount = 0;
+let playerScore = 0;
+let computerScore = 0;
+
+let playerSelection = "";
+let rock = document.querySelector("#rock");
+let paper = document.querySelector("#paper");
+let scissor = document.querySelector("#scissor");
+let score = document.querySelector("#score");
+let compScore = document.querySelector("#comp-score");
+
+score.innerText = playerScore;
+compScore.innerText = computerScore;
+
+let buttons = document.querySelectorAll('p');
+
+buttons.forEach((button) => {
+	button.addEventListener('click', (e) => {
+		playerSelection = e.target.innerText;
+		startRound();
+	});
+});
+
 function getComputerChoice() {
 	return gameValues[Math.floor(Math.random() * gameValues.length)]
 }
@@ -17,10 +40,11 @@ function getFirstCapital(word) {
 
 function playRound(computerSelection, playerSelection) {
 	alert(`Computer played ${computerSelection}`);
+	roundsCount += 1;
 	if (computerSelection == "Rock" && playerSelection == "Scissor" ||
 		computerSelection == "Scissor" && playerSelection == "Paper" ||
 		computerSelection == "Paper" && playerSelection == "Rock") {
-		return 0;
+		return -1;
 	} else if (computerSelection == playerSelection) {
 		return 0;
 	} else {
@@ -28,23 +52,25 @@ function playRound(computerSelection, playerSelection) {
 	}
 }
 
-function playBestOfFive() {
-	let finalScore = 0;
-	for (let i = 0; i < 5; i++) {
-		let playerSelection = getFirstCapital(prompt("ROCK, PAPER, SCISSOR"));
-		let computerSelection = getFirstCapital(getComputerChoice());
+function startRound() {
+	playerSelection = getFirstCapital(playerSelection);
+	let computerSelection = getFirstCapital(getComputerChoice());
 
-		let result = playRound(computerSelection, playerSelection);
-		if (result == 1) {
-			alert("You win this round!");
-			finalScore += 1;
+	let result = playRound(computerSelection, playerSelection);
+	if (result == 1) {
+		alert("You win this round!");
+		playerScore += 1;
+		score.innerText = playerScore;
+	} else if (result == -1) {
+		computerScore += 1;
+		compScore.innerText = computerScore;
+	}
+
+	if (roundsCount >= 5 && (playerScore >= 3 || computerScore >= 3)) {
+		if (playerScore >= 3) {
+			alert("You win the series!!");
+		} else {
+			alert("You Lose the series");
 		}
 	}
-	if (finalScore >= 3) {
-		alert("You win the series!!");
-	} else {
-		alert("You Lose the series");
-	}
 }
-
-playBestOfFive();
